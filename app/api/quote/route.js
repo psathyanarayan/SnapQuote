7c3a6ef302f009dd7245ea088f1397914b457727
Sathya Narayan <sathyanarayan@Sathyas-MacBook-Air.local>
2024-10-07 22:35:22 +0530

    Build fix

--- app/api/quote/route.js
+++ app/api/quote/route.js
@@ -1,17 +1,12 @@
 import { connectToDB } from "@utils/database";
 import Quote from "@models/quote";
 export const GET = async (req, res) => {
-  // Disable caching
-  res.setHeader(
-    "Cache-Control",
-    "no-store, no-cache, must-revalidate, proxy-revalidate"
-  );
-  res.setHeader("Pragma", "no-cache");
-  res.setHeader("Expires", "0");
-  res.setHeader("Surrogate-Control", "no-store");
+
 
   try {
+
     await connectToDB();
+    
     const allQuoteData = await Quote.find({}).populate("creator");
     return new Response(JSON.stringify(allQuoteData), { status: 201 });
   } catch (error) {
