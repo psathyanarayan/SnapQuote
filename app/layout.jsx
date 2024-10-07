a13f6c2d48aa6cb4ef35b832fc4a3fc9b226b1d9
Sathya Narayan <sathyanarayan@Sathyas-MacBook-Air.local>
2024-10-07 23:00:54 +0530

    Added Error Boundary

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 import "@styles/globals.css";
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
-
+import ErrorBoundary from "@components/ErrorBoundary";
 export const metadata = {
   title: "SnapQuote",
   description: "Unleash and Share",
@@ -10,15 +10,17 @@ export const metadata = {
 const RootLayout = ({ children }) => (
   <html lang="en">
     <body>
-      <Provider>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">
-          <Nav />
-          {children}
-        </main>
-      </Provider>
+      <ErrorBoundary fallback={<div>Something went wrong</div>}>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </ErrorBoundary>
     </body>
   </html>
 );
